32cafc1be856da8d6d48d2dc4b00589e347928e6
ma3obblu <Ma3oBblu@gmail.com>
2018-09-06 18:50:54 +0400

    [fix] json tests

--- tests/entities/UserMeta/UserMeta.php
+++ tests/entities/UserMeta/UserMeta.php
@@ -2,7 +2,10 @@
 namespace sorokinmedia\user\tests\entities\UserMeta;
 
 use sorokinmedia\user\entities\UserMeta\AbstractUserMeta;
+use sorokinmedia\user\entities\UserMeta\json\UserMetaPhone;
 use sorokinmedia\user\tests\entities\User\RelationClassTrait;
+use yii\db\Exception;
+use yii\helpers\Json;
 
 /**
  * Class UserMeta
@@ -11,4 +14,30 @@ use sorokinmedia\user\tests\entities\User\RelationClassTrait;
 class UserMeta extends AbstractUserMeta
 {
     use RelationClassTrait;
+
+    /**
+     * //TODO: приходится перегружать, т.к. json требует кодирования, декодирования. с MySQL проблем не замечено
+     * добавить номер телефона в профиль
+     * @param UserMetaPhone $userMetaPhone
+     * @return bool
+     * @throws Exception
+     */
+    public function setPhone(UserMetaPhone $userMetaPhone) : bool
+    {
+        $this->notification_phone = Json::encode($userMetaPhone);
+        return $this->updateModel();
+    }
+
+    /**
+     * верификация номер телефона
+     * @return bool
+     * @throws Exception
+     */
+    public function verifyPhone(): bool
+    {
+        $phone = new UserMetaPhone(Json::decode($this->notification_phone));
+        $phone->verifyPhone();
+        $this->notification_phone = Json::encode($phone);
+        return $this->updateModel();
+    }
 }
